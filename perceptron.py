dfb2fe2fa2dbab1e79cb06e8782cd6dc4bff751b
caspHa <caspar.hasner@gmail.com>
2017-09-25 15:59:38 +0200

    Implemented One-vs-All perceptron in such a way that the class produces csv files ready for submission to kaggle.

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,8 @@
 # This file implements the perceptron classifier
 
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
 
 
 class Perceptron(object):
@@ -53,4 +55,69 @@ class Perceptron(object):
         """ Return class label after unit step """
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-# TODO: extend class for multi-class classification, using One-vs.-All
+
+class PerceptronOvA(object):
+    """
+    Parameters
+    --------------------
+    eta - learning rate
+    n_epochs - passes over the training set
+
+    Attributes
+    --------------------
+    w_ - weights after fitting
+    b_ - bias after fitting
+    errors_ - number of misclassifications in every epoch
+    """
+
+    def __init__(self, eta=0.01, n_epochs=10):
+        self.eta = eta
+        self.n_epochs = n_epochs
+
+    def fit(self, X, y):
+        """
+        Parameters
+        --------------------
+        X - data
+        y - labels
+
+        Returns
+        --------------------
+        self
+        """
+
+        self.w_ = np.zeros(shape=(X.shape[1], len(y.unique())))
+        self.b_ = np.zeros(len(y.unique()))
+        lb = LabelBinarizer(neg_label=-1, pos_label=1)
+        self.yi_ = lb.fit_transform(y)
+
+        for _ in range(self.n_epochs):
+            for xi, target in zip(X.as_matrix(), self.yi_):
+                update = self.eta*(target - self.predict(xi))
+                self.w_ += np.outer(xi,update)
+                self.b_ += update
+        return self
+
+    def net_input(self, X):
+        """ Calculate net input function """
+        return np.dot(X, self.w_) + self.b_
+
+    def predict(self, X):
+        """ Return class label after unit step """
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+    
+    def gen_csv(self, X):
+        """ Generate a csv file with the ImageId and the Label """
+        prediction = np.argmax(self.predict(X), axis=1)
+        df_sub = pd.DataFrame({"ImageId": X.index.values+1, "Label": prediction})
+        return df_sub
+
+
+
+
+
+
+
+
+
+
